5dac5ad2e86d1a7aefb762ea5cde479f997d06ab
Jacin Labrador <49028521+jtlabs777@users.noreply.github.com>
2023-04-25 17:23:46 -0400

    Spring JPA - declared Todo class a @Entity , setup a primary key with random generated id value, and created a default schema called data.sql

--- myfirstwebapp/myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/Todo.java
+++ myfirstwebapp/myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/Todo.java
@@ -1,25 +1,34 @@
 package com.in28minutes.springboot.myfirstwebapp.todo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
 //Database (MYySQL)
 //Static List of todoes=> Database(h2, MySQL)
+
+@Entity
 public class Todo {
 
+    @Id
+    @GeneratedValue
     private int id;
+
     private String username;
 
     @Size(min=10, message="Enter at least 10 characters")
     private String description;
 
-    private LocalDate targetDate;
+    private LocalDate targetDate; //this shows up at TARGET_DATE in database because of the capital D
 
     private boolean done;
 
 
     public Todo(int id, String username, String description, LocalDate targetDate, boolean done) {
+        super();
         this.id = id;
         this.username = username;
         this.description = description;
---
+++ myfirstwebapp/myfirstwebapp/src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10001, 'jtlabs', 'GET AWS Certified', CURRENT_DATE(), false );
+
+INSERT INTO todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10002, 'jtlabs', 'GET Azure Certified', CURRENT_DATE(), false );
+
+INSERT INTO todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10003, 'jtlabs', 'GET GCP Certified', CURRENT_DATE(), false );
+
+INSERT INTO todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10004, 'jtlabs', 'Learn DevOps', CURRENT_DATE(), false );
+
