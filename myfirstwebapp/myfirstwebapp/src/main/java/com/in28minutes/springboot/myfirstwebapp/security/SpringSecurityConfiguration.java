2e6962090385bfb72fc54bd3ec03c5841ff22c8b
Jacin Labrador <49028521+jtlabs777@users.noreply.github.com>
2023-04-25 10:51:08 -0400

    added passwordEncoder, login fails , see notes on SpringSecurityConfiguration.java

--- myfirstwebapp/myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
+++ myfirstwebapp/myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration  //declare class as a bean to be used of Spring Security Context for configuraiton
@@ -24,5 +26,13 @@ public class SpringSecurityConfiguration {
                 .build();
 
         return new InMemoryUserDetailsManager(userDetails);
+    }
+
+    @Bean   //as it is right now, this will not let you login using deprecated feature
+    // because it is encrypting data user enters before the hash is checked
+    //the deprecated withDefaultPasswordEncoder feature in the createUserDetailsManager bean needs to be replaced
+    //with this encoder.
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
