26899c00c719f9130e222aa8d3c549ec8e0ee616
Jacin Labrador <49028521+jtlabs777@users.noreply.github.com>
2023-04-25 16:53:44 -0400

    Spring JPA and Security - configured DefaultSecuirtyFilterChain bean for compatibility with h2-console

--- myfirstwebapp/myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
+++ myfirstwebapp/myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
@@ -2,11 +2,15 @@ package com.in28minutes.springboot.myfirstwebapp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 import java.util.function.Function;
 
@@ -50,5 +54,17 @@ public class SpringSecurityConfiguration {
     //CSRF (Cross-site Request Forgery) needs to be disabled for h2-console to work
     //Frames are used by h2, spring security does not allow frames
 
+    @Bean
+    //SecurityWebFilterChain r DefaultSecuirtyFilterChain routes individuals to login when not authenticated
+    //this method will reconfigure
+    public DefaultSecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated());
 
+        http.formLogin(withDefaults());//configure form login so we can have a page where username and password can be collected
+
+        http.csrf().disable();//disable CSRF
+        http.headers().frameOptions().disable(); //enables use of frames on application
+        return http.build();
+    }
 }
